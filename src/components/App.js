f01bcd37e371ef0270c711fcef2a72c27f622449
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-08-05 15:11:57 +0000

    Submission1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
+import { useState,useEffect } from "react";
 
 const App = () => {
+  var timer;
+  let[count,setCount]=useState(0);
+  useEffect(()=>{
+    timer=setInterval(()=>{
+      setCount(count+1);
+    },1000)
+    return ()=>clearInterval(timer);
+  });
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>You've been on this page for {count} seconds</p>
     </div>
   )
 }
